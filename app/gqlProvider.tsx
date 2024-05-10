5e82a0d1d420c67eddef25e304e6dd5b2a670c6b
AvitusI <avitussweetbert213@gmail.com>
2024-05-10 22:46:09 +0300

    nextjs client graphql

--- app/gqlProvider.tsx
+++ app/gqlProvider.tsx
@@ -7,27 +7,27 @@ import {
   fetchExchange,
   createClient,
 } from '@urql/next'
-import { cacheExchange } from '@urql/exchange-graphcache'
+import { cacheExchange } from '@urql/exchange-graphcache' // like redux for graphql
 
 import { url } from '@/utils/url'
 import { getToken } from '@/utils/token'
 
 export default function GQLProvider({ children }: PropsWithChildren) {
-  const [client, ssr] = useMemo(() => {
-    const ssr = ssrExchange({
+  const [client, ssr] = useMemo(() => {  // Memorizes the function
+    const ssr = ssrExchange({  // ssrExchange makes sure it doesn't break on server
       isClient: typeof window !== 'undefined',
     })
 
     const client = createClient({
       url,
-      exchanges: [cacheExchange({}), ssr, fetchExchange],
+      exchanges: [cacheExchange({}), ssr, fetchExchange], // Plugins
       fetchOptions: () => {
         const token = getToken()
 
         return token
           ? {
-              headers: { authorization: `Bearer ${token}` },
-            }
+            headers: { authorization: `Bearer ${token}` }, // If you use cookies, you don't have to do this
+          }
           : {}
       },
     })
@@ -41,3 +41,5 @@ export default function GQLProvider({ children }: PropsWithChildren) {
     </UrqlProvider>
   )
 }
+
+// read about useReducer
